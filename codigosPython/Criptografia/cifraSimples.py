69f3dd82e38a42c70576de4b7818687946bc87a4
Joao <moreira.castor@gmail.com>
2025-02-14 23:14:14 -0300

    Criação do primeiro método de criptografia que será utilizado

---
+++ codigosPython/Criptografia/cifraSimples.py
@@ -0,0 +1,34 @@
+# Função que criptografe uma string com base na cifra Atbash
+# As frases devem ser escrita com as suas letras contrárias, a = z : b = y : c = x ..... 
+
+alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alfaInvert = alfabeto[::-1]
+
+desCripto = []
+
+
+def cripto(string="", selecao=0):
+    indexString = []
+    criptoString = []
+
+    # -------------Criptografia-------------
+    for x in string.lower():
+        indexString.append(alfaInvert.index(x))
+
+
+    if selecao == 0:
+        for x in indexString:
+            criptoString.append(alfabeto[x])
+
+        return "".join(criptoString)
+    else:
+        # -------------Descriptografia-------------
+        for x in indexString:
+            desCripto.append(alfaInvert[x])
+
+        return "".join(desCripto)
+
+print(cripto("JotaPe").capitalize())
+
+
+
